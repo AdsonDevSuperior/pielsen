8e70f9a9f9a60c7fda33f8074302f9a92e92e19d
Adson DevSuperior <adsonplayGG@gmail.com>
2021-04-24 23:28:19 -0300

    Starting

---
+++ praticandosemparar/src/main/java/com/cervejaria/PraticandosempararApplication.java
@@ -0,0 +1,13 @@
+package com.cervejaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandosempararApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandosempararApplication.class, args);
+	}
+
+}
---
+++ praticandosemparar/src/test/java/com/cervejaria/PraticandosempararApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cervejaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandosempararApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
